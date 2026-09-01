c6a0fecc42534d8322b9c42a671e48818e4ca6d1
肖智聪 <2095872515@qq.com>
2026-09-01 23:16:46 +0800

    提交1000000

--- examples/advanced_example.c
+++ examples/advanced_example.c
@@ -2,7 +2,7 @@
  * MultiButton Library Advanced Example
  * This example demonstrates advanced features and dynamic button management
  */
-10086
+10000000
 #include "multi_button.h"
 #include <stdio.h>
 #include <unistd.h>
